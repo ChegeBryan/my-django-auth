0b780759b0174134bf3f6e88cb03ccf11483c7f5
Chege Bryan <cbbryan8@gmail.com>
2020-07-30 12:39:48 +0300

    🎨 Create login page

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from users.views import dashboard
 
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('dashboard/', dashboard, name='dashboard'),
 ]
