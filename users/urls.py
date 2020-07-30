ff70dbc14d40bcc7beffb7af55e9cbda8619ac2d
Chege Bryan <cbbryan8@gmail.com>
2020-07-30 13:51:33 +0300

    🎨 Register new users

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from users.views import dashboard
+from users.views import dashboard, register
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('dashboard/', dashboard, name='dashboard'),
+    path('register/', register, name='register')
 ]
