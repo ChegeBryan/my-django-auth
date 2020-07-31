f3ff5b35a89c88924a741459f72ff6862a806707
Chege Bryan <cbbryan8@gmail.com>
2020-07-31 20:02:46 +0300

    🔗 Update urls

--- myauth/settings.py
+++ myauth/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['cbdjangoauth.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['cbdjangoauth.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
